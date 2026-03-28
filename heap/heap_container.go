448132d5980cbb11e2c7fa93c55dd1dbfb27a48a
Abdu Manaam <abdumanaam01@gmail.com>
2026-03-28 13:13:25 +0530

    heap problem

--- heap/heap_container.go
+++ heap/heap_container.go
@@ -1,4 +1,29 @@
 package heap
 
-import "container/heap"
 
+type InHeap []int 
+
+func(h InHeap)Len()int{
+   return len(h)
+}
+
+func(h InHeap)Less(i,j int)bool{
+	return h[i]<h[j]
+}
+
+func(h InHeap)Swap(i,j int){
+	h[i],h[j]=h[j],h[i]
+}
+
+func(h *InHeap)Push(x interface{}){
+   *h=append(*h,x.(int))
+}
+
+func(h *InHeap)Pop()interface{}{
+   
+	old:=*h
+	size:= len(old)
+	val:=old[size-1]
+	*h=(*h)[0:size-1]
+	return val
+}
--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"dsa/binary-tree"
-	"dsa/heap"
+	myheap "dsa/heap"
+	"container/heap"
 	"fmt"
 )
 
@@ -28,7 +29,7 @@ func main() {
 	fmt.Println("Search=",binarytree.Search(root,15))
 
     //HEAP
-	m := &heap.MinHeap{}
+	m := &myheap.MinHeap{}
 	s := []int{4, 5, 1, 2, 8, 9, 6, 3, 7}
 
 	for _, ch := range s {
@@ -38,5 +39,30 @@ func main() {
     fmt.Println("Heap Values:")
 	m.Display()
 	fmt.Println()
+
+	//Priority Queue
+	 h:=&myheap.InHeap{4,1,2,8,7}
+   heap.Init(h)
+   heap.Push(h, 10)
+   fmt.Printf("Min: %d\n",(*h)[0])
+   
+   copyHeap := make(myheap.InHeap, len(*h))
+    copy(copyHeap, *h)
+
+   fmt.Println("Priority Queue :")
+   for h.Len()>0{
+	fmt.Print(heap.Pop(h)," ")
+   }
+
+   // Shows elements in priority order
+    // Original heap remains unchanged
+    
+	fmt.Println()
+	
+    fmt.Println("Priority Queue :")
+    for copyHeap.Len() > 0 {
+    fmt.Println(heap.Pop(&copyHeap)," ")
+}
+
 
 }
